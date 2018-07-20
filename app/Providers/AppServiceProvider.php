9b1297b6d7b24108565042ea0fa4e3c7c997506c
unknown <semoncai@tnnet.gd>
2018-07-20 09:47:27 +0800

    Basic user profile editing

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,13 +7,14 @@ use Illuminate\Support\ServiceProvider;
 class AppServiceProvider extends ServiceProvider
 {
     /**
+     * AppServiceProvider 是框架的核心，在 Laravel 启动时，会最先加载该文件。
      * Bootstrap any application services.
      *
      * @return void
      */
     public function boot()
     {
-        //
+        \Carbon\Carbon::setLocale('zh');
     }
 
     /**
